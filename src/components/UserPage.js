2af33cda2bcd26232305cc3e75c6ea022fd94fd1
Timothy Do <tydo83@gmail.com>
2021-08-10 13:54:53 -0400

    updated weather api

--- src/components/UserPage.js
+++ src/components/UserPage.js
@@ -6,18 +6,11 @@ import { setFavoriteColorActionCreator, weatherApiCreator } from '../redux-state
 const UserPage = () => {
     const user = useSelector(state => state.user);
     const dispatch = useDispatch();
-
     const [favColor, setFavColor] = useState('red');
-    const [city, setCity] = useState('Seoul')
 
     const handleSetColor = () => {
         dispatch(setFavoriteColorActionCreator(favColor));
     };
-
-    const weatherApi = () => {
-        dispatch(weatherApiCreator(city));
-    }
-
 
     if (!user) {
         return <h1>No user</h1>
@@ -28,18 +21,14 @@ const UserPage = () => {
             {console.log(user)}
             <h1>Hi, {user.name}</h1>
             <h3>from {user.city}</h3>
+            <button onClick={() => dispatch(weatherApiCreator())}>Today' weather?</button>
+            <h3>Today's weather in {user.city} is {user.weather}</h3>
             <input
                 placeholder="favorite color"
                 value={favColor}
                 onChange={event => setFavColor(event.target.value)}
             />
             <button onClick={handleSetColor}>Set</button>
-            <input
-                placeholder="City"
-                value={city}
-                onChange={event => setCity(event.target.value)}
-            />
-            <button onClick={weatherApi}>Set</button>
         </div>
     )
 }
--- src/redux-state/userState.js
+++ src/redux-state/userState.js
@@ -1,8 +1,9 @@
+import axios from 'axios'
+
 // ACTIONS
 export const LOG_IN_ACTION = 'codeImmersives/logIn';
 export const SET_FAV_COLOR_ACTION = 'codeImmersives/setFavoriteColor';
 export const SEARCH_WEATHER = 'codeImmersives/searchWeather'
-export const REACT_APP_API_URL = 'https://api.openweathermap.org/data/2.5'
 export const REACT_APP_API_KEY = '18b8538e5eb115cc2512b683ed9f15cf'
 
 // ACTION CREATORS
@@ -14,21 +15,28 @@ export const loginActionCreator = (authorizedUser) => (dispatch, getState) => {
                 id: authorizedUser.id,
                 userName: authorizedUser.userName,
                 name: authorizedUser.name,
-                city: authorizedUser.city
+                city: authorizedUser.city,
             },
         })
     }, 1000)
 };
 
-export const weatherApiCreator = (authorizedUser) => (dispatch, getState) => {
-    setTimeout(() => {
+export const weatherApiCreator = (weather) => async (dispatch, getState) => {
+    let weather;
+    let city = getState().user.city
+    console.log(city);
+    try {
+        const response = await axios.get(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${REACT_APP_API_KEY}`);
+            weather =  response.data.weather[0].description;
+        } catch (error) {
+            console.error(error);
+        }
         dispatch({
             type: SEARCH_WEATHER,
             payload: {
-                city: authorizedUser.city
+                weather
             }
         })
-    }, 1000)
 }
 
 export const setFavoriteColorActionCreator = (color) => {
@@ -61,8 +69,10 @@ export const reducer = (state, action) => {
     }
 
     if (action.type === SEARCH_WEATHER) {
+        const weather = action.payload.weather
         return {
-            city: action.payload.city,
+            ...state,
+            weather,
         }
     }
 
